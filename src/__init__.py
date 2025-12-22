4cbff7afc18037c518013dbf1cb8a6a2846be8ea
Lumosylva <donnymoving@gmail.com>
2025-12-22 21:23:06 +0800

    feat(core): 初始化项目基础结构
    - 添加项目根目录初始化文件
    - 配置项目基本信息和元数据
    - 设置默认编码为UTF-8
    - 添加作者和联系方式信息
    - 创建项目描述占位符
    - 配置Python环境入口点

---
+++ src/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@ProjectName: homalos-data
+@FileName   : __init__.py.py
+@Date       : 2025/12/22 21:14
+@Author     : Lumosylva
+@Email      : donnymoving@gmail.com
+@Software   : PyCharm
+@Description: description
+"""
